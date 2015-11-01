0ad92585df0337bab18e6334fe4aba99045b2276
Magnus Rising <malytic@users.noreply.github.com>
2015-11-02 00:20:50 +0100

    Add infinite scroll and pattern generation

--- pwds.js
+++ pwds.js
@@ -11,18 +11,42 @@ chars['a'] = chars['l'] + chars['9'];
 chars['A'] = chars['U'] + chars['9'];
 chars['*'] = chars['l'] + chars['U'] + chars['9'] + chars['#'];
 
-function generate() {
-  var pattern = document.getElementById("pattern").value;
-  var ul = document.getElementById("passwords");
+var charKeys = Object.keys(chars);
+var keyRegex = new RegExp('[^' + charKeys + ']', 'g');
 
-  randomPasswords(pattern, 8).forEach(function(password) {
-    var li = document.createElement("li");
+var patternInput = document.getElementById('pattern');
+var patternLength = document.getElementById('pattern-length');
+var passwordList = document.getElementById('passwords');
+
+var onPatternChange = function() {
+  var sanitizedPattern = patternInput.value.replace(keyRegex, '');
+  patternInput.value = sanitizedPattern;
+  patternLength.innerHTML = sanitizedPattern.length;
+};
+
+var generatePattern = function() {
+  var i;
+  var possible = ['cv', 'CV', '99'];
+  var length = Math.floor(Math.random() * (7 - 4) + 4);
+  var pattern = new Array(length);
+  for (i = 0; i < length; i++) {
+    pattern.push(possible[Math.floor(Math.random() * possible.length)]);
+  }
+  patternInput.value = pattern.join('');
+  onPatternChange();
+};
+
+var appendPasswords = function(number) {
+  if (!patternInput.value.length) return;
+
+  generatePasswords(patternInput.value, number).forEach(function(password) {
+    var li = document.createElement('li');
     li.appendChild(document.createTextNode(password));
-    ul.insertBefore(li, ul.firstChild);
+    passwordList.appendChild(li);
   });
-}
+};
 
-function randomPasswords(pattern, number) {
+var generatePasswords = function(pattern, number) {
   var i, j, password, possible;
   var passwords = [];
 
@@ -35,4 +59,29 @@ function randomPasswords(pattern, number) {
     passwords.push(password.join(''));
   }
   return passwords;
-}
+};
+
+var fillViewport = function() {
+  var viewport = document.documentElement.clientHeight;
+  var docHeight = document.documentElement.scrollHeight;
+  if (viewport < docHeight) return;
+  var emptySpace = viewport - docHeight;
+  var passwordHeight = document.getElementById('li-test').scrollHeight;
+  var possiblePasswords = Math.ceil(emptySpace / passwordHeight);
+  appendPasswords(possiblePasswords + 5);
+};
+
+var scrollOptions = {
+  distance: 200,
+  callback: function(done) {
+    appendPasswords(10);
+    done();
+  }
+};
+infiniteScroll = new InfiniteScroll(scrollOptions);
+infiniteScroll.enable();
+
+generatePattern();
+fillViewport();
+
+window.addEventListener('resize', fillViewport);
---
+++ scroll.js
@@ -0,0 +1,61 @@
+var isIE = /msie/gi.test(navigator.userAgent);
+
+function getScrollPos() {
+  if (isIE) {
+    return document.documentElement.scrollTop;
+  } else {
+    return window.pageYOffset;
+  }
+}
+
+function InfiniteScroll(options) {
+  if (!(this instanceof InfiniteScroll)) {
+    return new InfiniteScroll(options);
+  }
+  options.callback = options.callback || function() {};
+  options.distance = options.distance || 50;
+
+  this.updateInitiated = false;
+  this.prevScrollPos = getScrollPos();
+  this.options = options;
+  this.enabled = false;
+
+  window.addEventListener('scroll',    this._onScroll.bind(this), false);
+  window.addEventListener('touchmove', this._onScroll.bind(this), false);
+}
+
+InfiniteScroll.prototype._onScroll = function(e) {
+  if (!this.enabled) return;
+  if (this.updateInitiated) return;
+
+  var scrollPos = getScrollPos();
+  if (scrollPos === this.prevScrollPos) return;
+
+  var body = document.body;
+  var html = document.documentElement;
+
+  var pageHeight = Math.max(
+    body.scrollHeight,
+    body.offsetHeight,
+    html.clientHeight,
+    html.scrollHeight,
+    html.offsetHeight
+  );
+  var clientHeight = document.documentElement.clientHeight;
+
+  if (pageHeight - (scrollPos + clientHeight) < this.options.distance) {
+    this.updateInitiated = true;
+    this.options.callback(function() {
+      this.updateInitiated = false;
+    }.bind(this));
+  }
+  this.prevScrollPos = scrollPos;
+};
+
+InfiniteScroll.prototype.enable = function() {
+  this.enabled = true;
+};
+
+InfiniteScroll.prototype.disable = function() {
+  this.enabled = false;
+};
